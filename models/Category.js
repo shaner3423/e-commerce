7142df2458d865d48d30bd3d2d3b88a72f5300aa
Sugar Shane <scrisostomo17@gmail.com>
2021-08-29 03:13:20 -0400

    fixed tag.js error

--- models/Category.js
+++ models/Category.js
@@ -13,7 +13,6 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-
     category_name: {
       type: DataTypes.SRING,
       allowNull: false,
--- models/Tag.js
+++ models/Tag.js
@@ -18,7 +18,7 @@ Tag.init(
       type: DataTypes.STRING
     }
   },
-  },
+  
   {
     sequelize,
     timestamps: false,
